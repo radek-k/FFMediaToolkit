48c83a2731a3fcaf61574408e58664137db2c8b7
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-05-05 16:05:30 +0200

    Add method for converting video frames to bitmap.

--- FFMediaToolkit/src/Common/Scaler.cs
+++ FFMediaToolkit/src/Common/Scaler.cs
@@ -37,6 +37,22 @@
         }
 
         /// <summary>
+        /// Converts a video <see cref="AVFrame"/> to bitmap data with a specified layout and writes its data to the specified memory buffer. Used in decoding.
+        /// </summary>
+        /// <param name="videoFrame">The video frame to convert</param>
+        /// <param name="videoLayout">The video frame layout</param>
+        /// <param name="destinationPointer">A pointer to the destination bitmap data buffer</param>
+        /// <param name="destinationLayout">The destination bitmap layout</param>
+        public void AVFrameToBitmap(AVFrame* videoFrame, Layout videoLayout, IntPtr destinationPointer, Layout destinationLayout)
+        {
+            var context = GetCachedContext(ref scaleContext, videoLayout, destinationLayout);
+            var ptr = (byte*)destinationPointer.ToPointer();
+            var data = new byte*[4] { ptr, null, null, null };
+            var linesize = new int[4] { destinationLayout.Stride, 0, 0, 0 };
+            ffmpeg.sws_scale(context, videoFrame->data, videoFrame->linesize, 0, videoLayout.Height, data, linesize);
+        }
+
+        /// <summary>
         /// Gets the <see cref="SwsContext"/> that can convert the <paramref name="source"/> layout to the <paramref name="destination"/> layout.
         /// </summary>
         /// <param name="cache">A field for use as object cache, to prevent unnecessary memory allocation. Can be <see langword="null"/>, it will be assigned with new object</param>
