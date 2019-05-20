8e61e42c94f0c13a449b381bebd5238dc8e6dc0d
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-05-20 16:07:27 +0200

    Changed the exception handling method.

--- FFMediaToolkit/src/Helpers/ExceptionHandler.cs
+++ FFMediaToolkit/src/Helpers/ExceptionHandler.cs
@@ -1,30 +1,23 @@
 ﻿namespace FFMediaToolkit.Helpers
 {
-    using System;
-    using System.Collections.Generic;
-    using FFmpeg.AutoGen;
-
     /// <summary>
     /// Contains common methods for handling FFMpeg exceptions
     /// </summary>
     internal static class ExceptionHandler
     {
         /// <summary>
-        /// Checks if specified integer is error code and throws an <see cref="FFMpegException"/>
+        /// Checks if specified integer is error code and throws an <see cref="FFmpegException"/>.
         /// </summary>
-        /// <param name="errorCode">Exit code returned by the FFMpeg method call</param>
-        /// <param name="desc">Information when method was called. Used to create the <see cref="FFMpegException"/> message</param>
-        internal static void ThrowIfError(this int errorCode, string desc)
+        /// <param name="errorCode">Exit code returned by the FFMpeg method call.</param>
+        /// <param name="message">The exception message.</param>
+        internal static void ThrowIfError(this int errorCode, string message)
         {
             if (errorCode >= 0)
             {
                 return;
             }
 
-            throw new FFMpegException(CreateMessage(errorCode, "ERR", desc));
+            throw new FFmpegException(message, errorCode);
         }
-
-        private static string CreateMessage(int errorCode, string error, string desc)
-            => $"An exception ocurred while {desc}. FFMpeg method call returned {error.ToLower()} error (code: {errorCode})";
     }
 }
