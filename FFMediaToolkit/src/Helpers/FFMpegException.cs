3723d7f748f59b53f500711249c1aa15b9e286d4
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-05-20 16:06:07 +0200

    Finish the FFmpegException.

--- FFMediaToolkit/src/Helpers/FFMpegException.cs
+++ FFMediaToolkit/src/Helpers/FFMpegException.cs
@@ -6,21 +6,46 @@
     /// Represents an exception thrown when FFMpeg method call returns an error code
     /// </summary>
     [Serializable]
-    public class FFMpegException : Exception
+    public class FFmpegException : Exception
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="FFMpegException"/> class.
+        /// Initializes a new instance of the <see cref="FFmpegException"/> class.
         /// </summary>
-        public FFMpegException() { }
+        public FFmpegException()
+        {
+        }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="FFMpegException"/> class with exception message
+        /// Initializes a new instance of the <see cref="FFmpegException"/> class using a message and a error code.
         /// </summary>
-        /// <param name="message">Exception message</param>
-        public FFMpegException(string message)
-            : base(message) { }
+        /// <param name="message">The exception message.</param>
+        public FFmpegException(string message)
+            : base(message)
+            => ErrorMessage = string.Empty;
 
-        public int? FFMpegErrorCode { get; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FFmpegException"/> class using a message and a error code.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        /// <param name="errorCode">The error code returned by the FFmpeg method.</param>
+        public FFmpegException(string message, int errorCode)
+            : base(CreateMessage(message, errorCode))
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = StringConverter.DecodeMessage(errorCode);
+        }
 
+        /// <summary>
+        /// Gets the error code returned by the FFmpeg method.
+        /// </summary>
+        public int? ErrorCode { get; }
+
+        /// <summary>
+        /// Gets the message text decoded from error code.
+        /// </summary>
+        public string ErrorMessage { get; }
+
+        private static string CreateMessage(string msg, int errCode)
+            => $"{msg} Error code: {errCode} : {StringConverter.DecodeMessage(errCode)}";
     }
 }
