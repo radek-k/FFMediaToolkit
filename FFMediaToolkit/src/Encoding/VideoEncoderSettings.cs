4aa073782087a62f6744518b3bb586209f2c6637
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-05-22 19:52:29 +0200

    Add missing periods.

--- FFMediaToolkit/src/Encoding/VideoEncoderSettings.cs
+++ FFMediaToolkit/src/Encoding/VideoEncoderSettings.cs
@@ -4,15 +4,15 @@
     using FFmpeg.AutoGen;
 
     /// <summary>
-    /// Represents a reusable video encoder configuration
+    /// Represents a reusable video encoder configuration.
     /// </summary>
     public class VideoEncoderSettings
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="VideoEncoderSettings"/> class with default video settings values;
+        /// Initializes a new instance of the <see cref="VideoEncoderSettings"/> class with default video settings values.
         /// </summary>
-        /// <param name="width">The video width</param>
-        /// <param name="height">The video height</param>
+        /// <param name="width">The video width.</param>
+        /// <param name="height">The video height.</param>
         public VideoEncoderSettings(int width, int height)
         {
             Bitrate = 30_000_000;
@@ -25,12 +25,12 @@
         }
 
         /// <summary>
-        /// Gets or sets the video stream bitrate (bytes per seconds). The default value is 30000000 B/s. If bitrate is too low, the result video will be pixelated
+        /// Gets or sets the video stream bitrate (bytes per seconds). The default value is 30000000 B/s. If bitrate is too low, the result video will be pixelated.
         /// </summary>
         public int Bitrate { get; set; }
 
         /// <summary>
-        /// Gets or sets the GoP value. The default value is 12. Higher value = lower file size
+        /// Gets or sets the GoP value. The default value is 12. Higher value = lower file size.
         /// </summary>
         public int KeyframeRate { get; set; }
 
@@ -46,12 +46,12 @@
 
         /// <summary>
         /// Gets or sets the output video pixel format. The default value is YUV420p.
-        /// Sended frames will be automatically converted to this format
+        /// Sended frames will be automatically converted to this format.
         /// </summary>
         public ImagePixelFormat VideoPixelFormat { get; set; }
 
         /// <summary>
-        /// Gets or sets video frame rate (FPS) value. The default value is 30 frames/s
+        /// Gets or sets video frame rate (FPS) value. The default value is 30 frames/s.
         /// </summary>
         public int Framerate { get; set; }
 
@@ -68,9 +68,9 @@
         public AVCodecID? Codec { get; set; }
 
         /// <summary>
-        /// Creates a <see cref="Layout"/> object, containg dimensions and pixel format, based on this settings
+        /// Creates a <see cref="Layout"/> object, containg dimensions and pixel format, based on this settings.
         /// </summary>
-        /// <returns>New <see cref="Layout"/> object</returns>
+        /// <returns>New <see cref="Layout"/> object.</returns>
         public Layout ToLayout() => new Layout((AVPixelFormat)VideoPixelFormat, VideoWidth, VideoHeight);
     }
 }
