640cb735c35872c7bafcece2b68ad2496142af9c
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-05-22 19:50:14 +0200

    Add the MediaOptions class.

---
+++ FFMediaToolkit/src/Decoding/MediaOptions.cs
@@ -0,0 +1,66 @@
+﻿namespace FFMediaToolkit.Decoding
+{
+    /// <summary>
+    /// Represents the audio/video streams loading modes.
+    /// </summary>
+    public enum MediaMode
+    {
+        /// <summary>
+        /// Enables loading both audio and video streams if exists.
+        /// </summary>
+        AudioVideo,
+
+        /// <summary>
+        /// Enables loading only video stream.
+        /// </summary>
+        Video,
+
+        /// <summary>
+        /// Enables loading only audio stream.
+        /// </summary>
+        Audio,
+    }
+
+    /// <summary>
+    /// Represents the multimedia file container options.
+    /// </summary>
+    public class MediaOptions
+    {
+        private const string Threads = "threads";
+        private const string Auto = "auto";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaOptions"/> class.
+        /// </summary>
+        /// <param name="streamsToLoad">The streams to load.</param>
+        public MediaOptions(MediaMode streamsToLoad)
+        {
+            DecoderThreads = null;
+            StreamsToLoad = streamsToLoad;
+        }
+
+        /// <summary>
+        /// Gets or sets the demuxer settings.
+        /// </summary>
+        public ContainerOptions DemuxerOptions { get; set; } = new ContainerOptions();
+
+        /// <summary>
+        /// Gets or sets which streams (audio/video) will be loaded.
+        /// </summary>
+        public MediaMode StreamsToLoad { get; set; } = MediaMode.AudioVideo;
+
+        /// <summary>
+        /// Gets or sets the number of decoder threads (by the 'threads' flag). The default value is <see langword="null"/> - 'auto'.
+        /// </summary>
+        public int? DecoderThreads
+        {
+            get => int.TryParse(DecoderOptions[Threads], out var count) ? (int?)count : null;
+            set => DecoderOptions[Threads] = value.HasValue ? value.ToString() : Auto;
+        }
+
+        /// <summary>
+        /// Gets or sets the dictionary with global options for the multimedia decoders.
+        /// </summary>
+        public FFDictionary DecoderOptions { get; set; } = new FFDictionary();
+    }
+}
