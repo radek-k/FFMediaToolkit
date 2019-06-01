94f988f71b6c67ec7e188874517492c6fb21f152
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-06-01 10:50:06 +0200

    Setup AppVeyor CI.

--- build.ps1
+++ build.ps1
@@ -11,6 +11,8 @@ function Exec
     }
 }
 
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
@@ -25,4 +27,4 @@ exec { & dotnet restore  }
 
 exec { & dotnet build  -c Release --version-suffix=$buildSuffix }
 
-exec { & dotnet pack .\FFMediaToolkit\FFMediaToolkit.csproj -c Release -o .\..\artifacts --include-symbols --no-build $versionSuffix }
+exec { & dotnet pack .\FFMediaToolkit\FFMediaToolkit.csproj -c Release -o .\artifacts --include-symbols --no-build $versionSuffix }
