4f2fdefb83232c7f49835fa0e8563763960372c3
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-06-13 15:21:36 +0200

    Redesing the VideoFrame and Scaler classes.

--- FFMediaToolkit/src/Common/Internal/Scaler.cs
+++ FFMediaToolkit/src/Common/Internal/Scaler.cs
@@ -1,6 +1,6 @@
 ﻿namespace FFMediaToolkit.Common.Internal
 {
-    using System;
+    using System.Drawing;
     using FFMediaToolkit.Graphics;
     using FFmpeg.AutoGen;
 
@@ -20,49 +20,44 @@
         /// <summary>
         /// Overrides the <paramref name="destinationFrame"/> image buffer with rescaled specified bitmap. Used in encoding.
         /// </summary>
-        /// <param name="bitmapPointer">Pointer to the input bitmap data.</param>
-        /// <param name="bitmapLayout">The input bitmap layout.</param>
+        /// <param name="bitmap">The input bitmap.</param>
         /// <param name="destinationFrame">The <see cref="AVFrame"/> to override.</param>
-        /// <param name="frameLayout">The output <see cref="AVFrame"/> layout setting.</param>
-        internal void FillAVFrame(IntPtr bitmapPointer, Layout bitmapLayout, AVFrame* destinationFrame, Layout frameLayout)
+        internal void FillAVFrame(BitmapData bitmap, VideoFrame destinationFrame)
         {
-            var context = GetCachedContext(bitmapLayout, frameLayout);
-            var ptr = (byte*)bitmapPointer.ToPointer();
-            var data = new byte*[4] { ptr, null, null, null };
-            var linesize = new int[4] { bitmapLayout.Stride, 0, 0, 0 };
-            ffmpeg.sws_scale(context, data, linesize, 0, bitmapLayout.Height, destinationFrame->data, destinationFrame->linesize);
+            var context = GetCachedContext(bitmap.ImageSize, (AVPixelFormat)bitmap.PixelFormat, destinationFrame.Layout, destinationFrame.PixelFormat);
+            fixed (byte* ptr = bitmap.Data)
+            {
+                var data = new byte*[4] { ptr, null, null, null };
+                var linesize = new int[4] { bitmap.Stride, 0, 0, 0 };
+                ffmpeg.sws_scale(context, data, linesize, 0, bitmap.ImageSize.Height, destinationFrame.Pointer->data, destinationFrame.Pointer->linesize);
+            }
         }
 
         /// <summary>
-        /// Converts a video <see cref="AVFrame"/> to bitmap data with a specified layout and writes its data to the specified memory buffer. Used in decoding.
+        /// Converts a video <see cref="AVFrame"/> to bitmap data with a specified layout and writes its to the specified <see cref="BitmapData"/>. Used in decoding.
         /// </summary>
         /// <param name="videoFrame">The video frame to convert.</param>
-        /// <param name="videoLayout">The video frame layout.</param>
-        /// <param name="destinationPointer">A pointer to the destination bitmap data buffer.</param>
-        /// <param name="destinationLayout">The destination bitmap layout.</param>
-        internal void AVFrameToBitmap(AVFrame* videoFrame, Layout videoLayout, IntPtr destinationPointer, Layout destinationLayout)
+        /// <param name="destination">The destination <see cref="BitmapData"/>.</param>
+        internal void AVFrameToBitmap(VideoFrame videoFrame, BitmapData destination)
         {
-            var context = GetCachedContext(videoLayout, destinationLayout);
-            var ptr = (byte*)destinationPointer.ToPointer();
-            var data = new byte*[4] { ptr, null, null, null };
-            var linesize = new int[4] { destinationLayout.Stride, 0, 0, 0 };
-            ffmpeg.sws_scale(context, videoFrame->data, videoFrame->linesize, 0, videoLayout.Height, data, linesize);
+            var context = GetCachedContext(videoFrame.Layout, videoFrame.PixelFormat, destination.ImageSize, (AVPixelFormat)destination.PixelFormat);
+            fixed (byte* ptr = destination.Data)
+            {
+                var data = new byte*[4] { ptr, null, null, null };
+                var linesize = new int[4] { destination.Stride, 0, 0, 0 };
+                ffmpeg.sws_scale(context, videoFrame.Pointer->data, videoFrame.Pointer->linesize, 0, videoFrame.Layout.Height, data, linesize);
+            }
         }
 
         /// <inheritdoc/>
         protected override void OnDisposing() => ffmpeg.sws_freeContext(Pointer);
 
-        private SwsContext* GetCachedContext(Layout source, Layout destination)
+        private SwsContext* GetCachedContext(Size sourceSize, AVPixelFormat sourceFormat, Size destinationSize, AVPixelFormat destinationFormat)
         {
-            if (source == destination)
-            {
-                return null;
-            }
-
             // If don't change the dimensions of the image, there is no need to use the high quality bicubic method.
-            var scaleMode = source.SizeEquals(destination) ? ffmpeg.SWS_POINT : ffmpeg.SWS_BICUBIC;
+            var scaleMode = sourceSize == destinationSize ? ffmpeg.SWS_POINT : ffmpeg.SWS_BICUBIC;
 
-            UpdatePointer(ffmpeg.sws_getCachedContext(Pointer, source.Width, source.Height, source.PixelFormat, destination.Width, destination.Height, destination.PixelFormat, scaleMode, null, null, null));
+            UpdatePointer(ffmpeg.sws_getCachedContext(Pointer, sourceSize.Width, sourceSize.Height, sourceFormat, destinationSize.Width, destinationSize.Height, destinationFormat, scaleMode, null, null, null));
             return Pointer;
         }
     }
--- FFMediaToolkit/src/Common/Internal/VideoFrame.cs
+++ FFMediaToolkit/src/Common/Internal/VideoFrame.cs
@@ -1,6 +1,7 @@
 ﻿namespace FFMediaToolkit.Common.Internal
 {
     using System;
+    using System.Drawing;
     using FFMediaToolkit.Graphics;
     using FFMediaToolkit.Helpers;
     using FFmpeg.AutoGen;
@@ -24,20 +25,26 @@
         /// <summary>
         /// Gets the frame dimensions.
         /// </summary>
-        public Layout Layout => Pointer != null ? new Layout((AVPixelFormat)Pointer->format, Pointer->width, Pointer->height) : default;
+        public Size Layout => Pointer != null ? new Size(Pointer->width, Pointer->height) : default;
+
+        /// <summary>
+        /// Gets the frame pixel format.
+        /// </summary>
+        public AVPixelFormat PixelFormat => Pointer != null ? (AVPixelFormat)Pointer->format : default;
 
         /// <summary>
         /// Creates a video frame with given dimensions and allocates a buffer for it.
         /// </summary>
         /// <param name="dimensions">The dimensions of the video frame.</param>
+        /// <param name="pixelFormat">The video pixel format.</param>
         /// <returns>The new video frame.</returns>
-        public static VideoFrame Create(Layout dimensions)
+        public static VideoFrame Create(Size dimensions, AVPixelFormat pixelFormat)
         {
             var frame = ffmpeg.av_frame_alloc();
 
             frame->width = dimensions.Width;
             frame->height = dimensions.Height;
-            frame->format = (int)dimensions.PixelFormat;
+            frame->format = (int)pixelFormat;
 
             ffmpeg.av_frame_get_buffer(frame, 32);
 
@@ -55,13 +62,7 @@
         /// </summary>
         /// <param name="bitmap">The bitmap to convert.</param>
         /// <param name="scaler">A <see cref="Scaler"/> object, used for caching the FFMpeg <see cref="SwsContext"/> when converting many frames of the same video.</param>
-        public void UpdateFromBitmap(BitmapData bitmap, Scaler scaler)
-        {
-            fixed (byte* ptr = bitmap.Data)
-            {
-                scaler.FillAVFrame((IntPtr)ptr, bitmap.Layout, Pointer, Layout);
-            }
-        }
+        public void UpdateFromBitmap(BitmapData bitmap, Scaler scaler) => scaler.FillAVFrame(bitmap, this);
 
         /// <summary>
         /// Converts this video frame to the <see cref="BitmapData"/> with the specified pixel format.
@@ -71,14 +72,8 @@
         /// <returns>A <see cref="BitmapData"/> instance containg converted bitmap data.</returns>
         public BitmapData ToBitmap(Scaler scaler, ImagePixelFormat targetFormat)
         {
-            var bitmap = BitmapData.CreatePooled(Layout.Width, Layout.Height, targetFormat);
-            var targetLayout = new Layout((AVPixelFormat)targetFormat, Layout.Width, Layout.Height);
-
-            fixed (byte* ptr = bitmap.Data)
-            {
-                scaler.AVFrameToBitmap(Pointer, Layout, (IntPtr)ptr, targetLayout);
-            }
-
+            var bitmap = BitmapData.CreatePooled(Layout, targetFormat);
+            scaler.AVFrameToBitmap(this, bitmap);
             return bitmap;
         }
 
