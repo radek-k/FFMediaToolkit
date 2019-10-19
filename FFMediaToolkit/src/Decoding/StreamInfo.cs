f95b6bb658722239f72e88546cfe27e93e26bff9
Marcos Cordeiro <mvmcjunior@gmail.com>
2019-10-19 12:26:44 -0300

    Adds StreamChapter support
    This parses all "chapters" for a stream info.

--- FFMediaToolkit/src/Decoding/StreamInfo.cs
+++ FFMediaToolkit/src/Decoding/StreamInfo.cs
@@ -4,6 +4,7 @@
     using System.Collections.ObjectModel;
     using System.Drawing;
     using FFMediaToolkit.Common;
+    using FFMediaToolkit.Decoding.Internal;
     using FFMediaToolkit.Helpers;
     using FFmpeg.AutoGen;
 
@@ -23,7 +24,8 @@
             CodecName = ffmpeg.avcodec_get_name(codec->codec_id);
             CodecId = FormatCodecId(codec->codec_id);
             Index = stream->index;
-            IsInterlaced = codec->field_order != AVFieldOrder.AV_FIELD_PROGRESSIVE && codec->field_order != AVFieldOrder.AV_FIELD_UNKNOWN;
+            IsInterlaced = codec->field_order != AVFieldOrder.AV_FIELD_PROGRESSIVE &&
+                           codec->field_order != AVFieldOrder.AV_FIELD_UNKNOWN;
             FrameSize = new Size(codec->width, codec->height);
             PixelFormat = codec->pix_fmt;
             TimeBase = stream->time_base;
@@ -33,6 +35,23 @@
             var start = stream->start_time.ToTimeSpan(stream->time_base);
             StartTime = start == TimeSpan.MinValue ? TimeSpan.Zero : start;
             FrameCount = Duration.ToFrameNumber(RFrameRate);
+            Chapters = ParseChapters(container);
+        }
+
+        private unsafe StreamChapter[] ParseChapters(InputContainer container)
+        {
+            var streamChapters = new StreamChapter[container.Pointer->nb_chapters];
+
+            for (var i = 0; i < container.Pointer->nb_chapters; i++)
+            {
+                var chapter = container.Pointer->chapters[i];
+                var meta = chapter->metadata;
+                var startTimespan = TimeSpan.FromTicks(chapter->start / 100);
+                var endTimespan = TimeSpan.FromTicks(chapter->end / 100);
+                Chapters[i] = new StreamChapter(startTimespan, endTimespan, FFDictionary.ToDictionary(meta, true));
+            }
+
+            return streamChapters;
         }
 
         /// <summary>
@@ -99,6 +118,11 @@
         /// Gets the stream metadata.
         /// </summary>
         public ReadOnlyDictionary<string, string> Metadata { get; }
+
+        /// <summary>
+        /// Gets the stream chapters.
+        /// </summary>
+        public StreamChapter[] Chapters { get; }
 
         private static string FormatCodecId(AVCodecID id) => id.ToString().Substring(12).ToLower();
     }
