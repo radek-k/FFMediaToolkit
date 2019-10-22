c3c6811b98d25e5106355f411d9472a0d9a7fb0d
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-10-22 16:22:58 +0200

    Rename the Scaler class.

--- FFMediaToolkit/src/Common/Internal/VideoFrame.cs
+++ FFMediaToolkit/src/Common/Internal/VideoFrame.cs
@@ -58,19 +58,19 @@
         public static VideoFrame CreateEmpty() => new VideoFrame(ffmpeg.av_frame_alloc());
 
         /// <summary>
-        /// Overrides this video frame data with the converted <paramref name="bitmap"/> using specified <see cref="Scaler"/> object.
+        /// Overrides this video frame data with the converted <paramref name="bitmap"/> using specified <see cref="ImageConverter"/> object.
         /// </summary>
         /// <param name="bitmap">The bitmap to convert.</param>
-        /// <param name="scaler">A <see cref="Scaler"/> object, used for caching the FFMpeg <see cref="SwsContext"/> when converting many frames of the same video.</param>
-        public void UpdateFromBitmap(ImageData bitmap, Scaler scaler) => scaler.FillAVFrame(bitmap, this);
+        /// <param name="scaler">A <see cref="ImageConverter"/> object, used for caching the FFMpeg <see cref="SwsContext"/> when converting many frames of the same video.</param>
+        public void UpdateFromBitmap(ImageData bitmap, ImageConverter scaler) => scaler.FillAVFrame(bitmap, this);
 
         /// <summary>
         /// Converts this video frame to the <see cref="ImageData"/> with the specified pixel format.
         /// </summary>
-        /// <param name="scaler">A <see cref="Scaler"/> object, used for caching the FFMpeg <see cref="SwsContext"/> when converting many frames of the same video.</param>
+        /// <param name="scaler">A <see cref="ImageConverter"/> object, used for caching the FFMpeg <see cref="SwsContext"/> when converting many frames of the same video.</param>
         /// <param name="targetFormat">The output bitmap pixel format.</param>
         /// <returns>A <see cref="ImageData"/> instance containg converted bitmap data.</returns>
-        public ImageData ToBitmap(Scaler scaler, ImagePixelFormat targetFormat)
+        public ImageData ToBitmap(ImageConverter scaler, ImagePixelFormat targetFormat)
         {
             var bitmap = ImageData.CreatePooled(Layout, targetFormat);
             scaler.AVFrameToBitmap(this, bitmap);
