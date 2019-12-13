eb231770fc11d4a345f3aee044e7cd73b92ef80b
Radosław Kmiotek <rk58690poczta@outlook.com>
2019-12-13 17:10:33 +0100

    Add a log verbosity level setting.

---
+++ FFMediaToolkit/src/LogLevel.cs
@@ -0,0 +1,35 @@
+﻿namespace FFMediaToolkit
+{
+    using FFmpeg.AutoGen;
+
+    /// <summary>
+    /// FFMpeg logging verbosity levels.
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Doesn't print any messages.
+        /// </summary>
+        Quiet = ffmpeg.AV_LOG_QUIET,
+
+        /// <summary>
+        /// Prints only error messages.
+        /// </summary>
+        Error = ffmpeg.AV_LOG_ERROR,
+
+        /// <summary>
+        /// Prints error and warning messages.
+        /// </summary>
+        Warning = ffmpeg.AV_LOG_WARNING,
+
+        /// <summary>
+        /// Prints errors, warnings and informational messages.
+        /// </summary>
+        Info = ffmpeg.AV_LOG_INFO,
+
+        /// <summary>
+        /// Prints the most detailed messages.
+        /// </summary>
+        Verbose = ffmpeg.AV_LOG_VERBOSE,
+    }
+}
