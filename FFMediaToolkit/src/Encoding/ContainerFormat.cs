330971dbcc07f53c66d52e3051368c1df6b79d14
Radosław Kmiotek <rk58690poczta@outlook.com>
2020-05-18 16:39:36 +0200

    Add the ContainerFormat enum for encoding.

---
+++ FFMediaToolkit/src/Encoding/ContainerFormat.cs
@@ -0,0 +1,49 @@
+﻿namespace FFMediaToolkit.Encoding
+{
+    /// <summary>
+    /// Video container formats supported by FFMediaToolkit.
+    /// </summary>
+    public enum ContainerFormat
+    {
+        /// <summary>
+        /// The 3GPP container (.3gp)
+        /// </summary>
+        ThridGPP,
+
+        /// <summary>
+        /// The 2nd 3GPP container (.3g2)
+        /// </summary>
+        ThridGPP2,
+
+        /// <summary>
+        /// The Microsoft Advanced Systems Formats container (.asf)
+        /// Use this container when encoding a .wmv (Windows Media) video file.
+        /// </summary>
+        ASF,
+
+        /// <summary>
+        /// The Audio Video Interleave containter (.avi)
+        /// </summary>
+        AVI,
+
+        /// <summary>
+        /// The Matroska Multimedia Container (.mkv)
+        /// </summary>
+        MKV,
+
+        /// <summary>
+        /// The MPEG-4 container (.mp4)
+        /// </summary>
+        MP4,
+
+        /// <summary>
+        /// The Ogg container (.ogv for video files)
+        /// </summary>
+        Ogg,
+
+        /// <summary>
+        /// The WebM container (.webm)
+        /// </summary>
+        WebM,
+    }
+}
